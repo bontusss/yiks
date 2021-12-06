03728e60cd20e4696f03ce27e929126a51859603
bontusss <ikwecheghu@gmail.com>
2021-12-06 22:29:08 +0100

    changed api url to production url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  // API_URL: "https://thebontus.herokuapp.com/api/v1/",
-  API_URL: "http://localhost:8000/api/v1/",
+  API_URL: "https://thebontus.herokuapp.com/api/v1/",
+  // API_URL: "http://localhost:8000/api/v1/",
 };
 
 export default Config;
